4a051716fef0fd01fd3085daf512af4385d3f4cd
Alessio Bogon <youtux@gmail.com>
2019-02-05 16:09:55 +0100

    Initial commit, nothing to see here

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Alessio Bogon <https://github.com/youtux>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup(
+    author="Alessio Bogon",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="File System Backend for Dogpile Cache",
+    install_requires=['dogpile.cache'],
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='dogpile_cache_file_system_backend',
+    name='dogpile_cache_file_system_backend',
+    packages=find_packages(include=['dogpile_cache_file_system_backend']),
+    test_suite='tests',
+    url='https://github.com/youtux/dogpile_cache_file_system_backend',
+    version='0.1.0',
+    zip_safe=False,
+)
