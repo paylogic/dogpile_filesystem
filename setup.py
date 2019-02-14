d56bbcf34115e403fbc3e3e48aa8091207ab5320
Alessio Bogon <youtux@gmail.com>
2019-02-14 10:26:34 +0100

    Improve tox config

--- setup.py
+++ setup.py
@@ -14,14 +14,13 @@ with open('HISTORY.rst') as history_file:
 setup(
     author="Alessio Bogon",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 2 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
