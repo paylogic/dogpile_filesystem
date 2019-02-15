4cf5cd450b89882fec22ce5f79ba23bf918b822d
Alessio Bogon <youtux@gmail.com>
2019-02-15 12:20:44 +0100

    Tell PyPI we are using markdown

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     ],
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='dogpile_filesystem',
     name='dogpile_filesystem',
