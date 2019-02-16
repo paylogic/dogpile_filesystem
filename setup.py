ba0ce441eb1c3b4e0c984fc2be31ba737f09661a
Alessio Bogon <youtux@gmail.com>
2019-02-16 20:19:12 +0100

    Add pypy implementation to setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,9 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     description="File System Backends for Dogpile Cache",
     install_requires=[
